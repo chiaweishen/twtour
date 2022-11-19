96aef2ca44a3f8545e001c8c2f2a2f6b6c497b2c
ethanshen <ethan.scw@gmail.com>
2022-11-20 00:53:37 +0800

    Refactor

--- app/src/main/java/com/scw/twtour/view/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/scw/twtour/view/viewmodel/HomeViewModel.kt
@@ -8,28 +8,29 @@ import com.scw.twtour.domain.ScenicSpotUseCase
 import com.scw.twtour.model.data.HomeListItem
 import com.scw.twtour.model.data.Result
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val scenicSpotUseCase: ScenicSpotUseCase
 ) : ViewModel() {
 
-    private val _listItems: MutableLiveData<Result<List<HomeListItem>>> = MutableLiveData(Result.Loading)
+    private val _listItems: MutableLiveData<Result<List<HomeListItem>>> =
+        MutableLiveData(Result.Loading)
     val listItems: LiveData<Result<List<HomeListItem>>> get() = _listItems
 
     fun fetchScenicSpotItems() {
-        viewModelScope.launch {
-            scenicSpotUseCase.fetchScenicSpotItems()
-                .onStart {
-                    _listItems.value = Result.Loading
-                }
-                .catch { e ->
-                    _listItems.value = Result.Error(Exception(e))
-                }
-                .collect { items ->
-                    _listItems.value = Result.Success(items)
-                }
-        }
+        scenicSpotUseCase.fetchScenicSpotItems()
+            .onStart {
+                _listItems.value = Result.Loading
+            }
+            .onEach { items ->
+                _listItems.value = Result.Success(items)
+            }
+            .catch { e ->
+                _listItems.value = Result.Error(Exception(e))
+            }
+            .launchIn(viewModelScope)
     }
 }
